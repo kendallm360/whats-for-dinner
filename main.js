9d10ad8185f916da78cf98c82265d55287290378
Kendall McGree <mcgreekendall@gmail.com>
2022-02-17 18:20:10 -0800

    Add Button Functionality

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ var desserts = [
   "Truffle Butter",
   "Tres Leches",
   "Bread Pudding",
-  "Brownie",
+  "CookieBrownie",
   "Apple Pie",
   "Sweet Potato Pie",
   "Tart",
@@ -48,13 +48,59 @@ var desserts = [
   "Strawberry Cake",
 ];
 
+var cookButton = document.querySelector(".cook");
+var optionBox = document.querySelector(".option-box");
+var log = document.querySelector("#log");
 var sideDishButton = document.querySelector("#side");
+var recipeBox = document.querySelector(".center-pic");
 
-sideDishButton.addEventListener("click", selectSideDish);
+//optionBox.addEventListener("submit" /* ???function(event)??? */);
+cookButton.addEventListener("click", showDish);
+//sideDishButton.addEventListener("click", selectSideDish);
 
-selectSideDish();
-var newSide = sides[getRandomInfo(sides)];
-// displayCover(newCover, newTitle, newDescriptor1, newDescriptor2)
+// var radios = document.getElementsByName("dish-selection");
+//
+// for (var i = 0, length = radios.length; i < length; i++) {
+//   if (radios[i].checked) {
+//     console.log(radios[i].value);
+//   }
+// }
+
+function showDish() {
+  // var dishPick = document.querySelector('input[name="dishPick"]:checked').value;
+  // var radioSelection = dishPick.value;
+
+  var radios = document.getElementsByName("dish-selection");
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      var steve = radios[i].value;
+    }
+  }
+  console.log(steve);
+  if (steve === "side") {
+    recipeBox.innerHTML = `
+    <div class="option-selected">
+    <p>You should make:</p>
+    <p>${sides[getRandomInfo(sides)]}</p>
+    </div>`;
+  }
+}
+
+function selectSideDish() {
+  var newSide = sides[getRandomInfo(sides)];
+  return newSide;
+  // console.log(sides);
+  // displayDish(newCover, newTitle, newDescriptor1, newDescriptor2)
+}
+
+function displayDish(newDish) {
+  currentCover = new Cover(newCover, newTitle, newDescriptor1, newDescriptor2);
+  currentTitle.innerText = newTitle;
+  currentImage.src = newCover;
+  currentTagLine1.innerText = newDescriptor1;
+  currentTagLine2.innerText = newDescriptor2;
+}
 
 function getRandomInfo(dish) {
   return Math.floor(Math.random() * dish.length);
